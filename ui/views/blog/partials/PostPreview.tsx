205d7f622bcb862bc2f8ef84f3f96695eb9d3a12
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2021-02-13 17:15:57 -0600

    standardizing react import

--- ui/views/blog/partials/PostPreview.tsx
+++ ui/views/blog/partials/PostPreview.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Timestamp from '../../components/Timestamp';
 import PrismicDOM from 'prismic-dom';
 
--- ui/views/components/Timestamp.tsx
+++ ui/views/components/Timestamp.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 export default class Timestamp extends React.Component<{
   dateTime: Date | string | number;
