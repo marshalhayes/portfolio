7fe38aedc9d9550a43cb0827e1022cf366a099f1
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2021-02-14 23:28:25 -0600

    only using cssnano in prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,20 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: {
-      preset: 'default',
-    },
+const plugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+};
+
+const prodPlugins = {
+  cssnano: {
+    preset: 'default',
   },
+};
+
+if (process.env.NODE_ENV === 'production') {
+  Object.assign(plugins, { ...plugins, ...prodPlugins });
+
+  console.log(plugins);
+}
+
+module.exports = {
+  plugins,
 };
