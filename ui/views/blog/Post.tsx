f94e6860ee62bf7875d7fef32e7fbeaaea265c58
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2021-02-19 22:42:06 -0600

    moving prism.css to a min file and into a vendor folder

--- ui/views/blog/Post.tsx
+++ ui/views/blog/Post.tsx
@@ -89,7 +89,7 @@ export default class Post extends React.Component<PostProps> {
           ) : null}
         </div>
 
-        <link rel="stylesheet" href="/public/css/prism.css" />
+        <link rel="stylesheet" href="/public/css/vendor/prism.css" />
 
         {this.props.isPreview ? (
           <script
